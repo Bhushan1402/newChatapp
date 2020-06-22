afb33ee464edbbef071a27fda1caf1d007188302
Bhushan1402 <62240814+Bhushan1402@users.noreply.github.com>
2020-06-23 00:47:32 +0530

    Update settings.py

--- fundoo/fundoo/settings.py
+++ fundoo/fundoo/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
         'NAME': 'ChatApp',
         'USER': 'bhushan',
         'PASSWORD': 'Bhushan123',
-        'HOST': 'terraform-20200622052221982000000001.cp8ezss9fwvy.us-east-2.rds.amazonaws.com',
+        'HOST': 'db.cp8ezss9fwvy.us-east-2.rds.amazonaws.com',
         'PORT': '5432',
     }
 }
