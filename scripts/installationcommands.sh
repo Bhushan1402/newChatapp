b4174b63d29b214b4017f693a042946352796df4
Bhushan1402 <62240814+Bhushan1402@users.noreply.github.com>
2020-07-01 10:55:16 +0530

    Update installationcommands.sh

--- scripts/installationcommands.sh
+++ scripts/installationcommands.sh
@@ -2,21 +2,21 @@
 sudo apt-get update
 sudo apt-get install python3-pip3 python3-dev nginx git -y
 sudo apt-get update
-cd /new_chatapp
+cd /newChatapp
 pip3 install virtualenv
 virtualenv venv
-source /new_chatapp/venv/bin/activate
+source /newChatapp/venv/bin/activate
 pip3 install -r requirements.txt
 pip3 install django bcrypt django-extensions
 pip3 install gunicorn
 cd fundoo/
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate
-sudo cp /new_chatapp/scripts/gunicorn.service /etc/systemd/system/
+sudo cp /newChatapp/scripts/gunicorn.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start gunicorn
 sudo systemctl enable gunicorn
 sudo unlink /etc/nginx/sites-enabled/*
-sudo cp /new_chatapp/scripts/fundoo /etc/nginx/sites-available/
+sudo cp /newChatapp/scripts/fundoo /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/fundoo /etc/nginx/sites-enabled
 sudo systemctl restart nginx
